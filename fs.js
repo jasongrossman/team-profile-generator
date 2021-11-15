a95503367dd3611ff031063c97c22a05cb8d54b9
jasongrossman <85508030+jasongrossman@users.noreply.github.com>
2021-11-15 17:02:49 -0500

    formatted HTML and CSS to enhance display

--- fs.js
+++ fs.js
@@ -20,12 +20,14 @@ const generateHTMLTemplate = (allTeamMembers) => {
     manager.getRole();
   
     let managerCard = 
-    `<div className="manager-container">
-      <p>${manager.getName()}</p>
-      <p> ${manager.getId()}</p>
-      <p> ${manager.getEmail()}</p>
-      <p>${manager.getOfficeNumber()}</p>
-      <p>${manager.getRole()}</p>
+    `<div class="card" style="width: 18 rem;">
+      <h1 class="card-title">${manager.getName()}</h1>
+      <h2 class="card-subtitle">${manager.getRole()}</h2>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">ID: ${manager.getId()}</li>
+        <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+        <li class="list-group-item"> Office Number: ${manager.getOfficeNumber()}</li>
+      </ul>
     </div>`
 
     teamHTML.push(managerCard);  
@@ -33,24 +35,29 @@ const generateHTMLTemplate = (allTeamMembers) => {
 
   const createEngineer = (engineer) => {
     let engineerCard = 
-    `<div className="engineer-container">
-    <p> ${engineer.getName()}</p>
-    <p>${engineer.getId()}</p>
-    <p>${engineer.getEmail()}</p>
-    <p>${engineer.getGithub()}</p>
-    <p>${engineer.getRole()}</p>
+    `<div class="card" style="width: 18 rem;">
+      <h1 class="card-title">${engineer.getName()}</h1>
+      <h2 class="card-subtitle">${engineer.getRole()}</h2>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">ID: ${engineer.getId()}</li>
+        <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+        <li class="list-group-item"> Github:<a href="https://github.com/${engineer.getGithub()}"> ${engineer.getGithub()}</a></li>
+      </ul>
     </div>`
+
     teamHTML.push(engineerCard);  
   }
 
   const createIntern = (intern) => {
     let internCard = 
-    `<div className="intern-container">
-      ${intern.getName()};
-      ${intern.getId()};
-      ${intern.getEmail()};
-      ${intern.getSchool()};
-      ${intern.getRole()};
+    `<div class="card mb-3" style="width: 18 rem;">
+      <h1 class="card-title">${intern.getName()}</h1>
+      <h2 class="card-subtitle">${intern.getRole()}</h2>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">ID: ${intern.getId()}</li>
+        <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
+        <li class="list-group-item">School: ${intern.getSchool()}</a></li>
+      </ul>
     </div>`
     teamHTML.push(internCard);  
   }
@@ -81,11 +88,16 @@ const generateHTMLTemplate = (allTeamMembers) => {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">  
+    <link rel="stylesheet" href="./dist/style.css">
   </head>
   
   <body>
+      <header> 
+        My Team
+      </header>
       <main>
-         ${tempHTML};
+         ${tempHTML}
       </main>
   
   </body>
@@ -95,23 +107,5 @@ const generateHTMLTemplate = (allTeamMembers) => {
   </footer>`
     return template;
 } 
-
-
-
-// module.exports = myTeam();
-
-
-
-// const names = [ 'bill', 'jane', 'joe'];
-
-// let data = '';
-
-// for(var i = 0; i < names.length; i ++){
-//     data = data + names[i] ;
-// }
-
-  
-
-
 
 module.exports = generateHTMLTemplate;
